46fb964e9eefe0c3056cf10eeb72bef945cc1bc3
Alexandr Kolushkin <kolushkin@gmail.com>
2025-10-19 16:43:24 +0300

    homework1: hw02-gradle

--- hw01-gradle/src/main/java/ru/otus/HelloOtus.java
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -6,11 +6,11 @@ import java.util.List;
 @SuppressWarnings("java:S106")
 public class HelloOtus {
     public static void main(String... args) {
-        var words = List.of("Hello", "Otus");
+        var words = List.of("Hello", "Otus", "Guava");
 
-        // 2️⃣ Склеиваем элементы списка через пробел
+        // Склеиваем элементы списка через пробел
         String result = Joiner.on(" ").join(words);
 
-        System.out.println(result); // Выведет: Hello Guava World
+        System.out.println(result); // Выведет: Hello Otus Guava
     }
 }
