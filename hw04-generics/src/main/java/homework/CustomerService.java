fee09e13b8381756d7de4adb06e601aa7ab03b40
Alexandr Kolushkin <kolushkin@gmail.com>
2025-10-22 08:53:08 +0300

    homework1: hw02-gradle

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class CustomerService {
 
     // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
-    private final TreeMap<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
+    private final NavigableMap<Customer, String> customers = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
         // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map. Entry сделан в jdk
