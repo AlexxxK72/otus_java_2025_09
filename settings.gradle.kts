b0f66153bca49e64c708c3d74cb70b15792c6543
Alexandr Kolushkin <kolushkin@gmail.com>
2026-02-24 19:23:33 +0300

    hw32-concurrentCollections

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,8 @@ include("hw22-cache")
 include("hw24-webServer")
 include("hw25-di")
 include("hw28-springDataJdbc")
+include("hw31-executors")
+include("hw32-concurrentCollections")
 
 pluginManagement {
     val jgitver: String by settings
@@ -37,4 +39,3 @@ pluginManagement {
     }
 }
 
-include("hw31-executors")
